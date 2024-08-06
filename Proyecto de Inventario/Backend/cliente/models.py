afce31311f8a3884bb054dd7856ea9ee36849093
davidmateus1202 <mateusdavid417@gmail.com>
2024-08-06 09:39:07 -0500

    modelo corregido

--- Proyecto de Inventario/Backend/cliente/models.py
+++ Proyecto de Inventario/Backend/cliente/models.py
@@ -7,7 +7,7 @@ class Cliente(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=1000)
     tipo_id = models.CharField(max_length=1000)
-    identificacion = models.CharField(max_length=1000)
+    identificacion = models.CharField(max_length=1000, unique=True)
     telefono = models.CharField(max_length=1000, null=True, blank=True)
     email = models.CharField(max_length=1000, null=True, blank=True)
 
